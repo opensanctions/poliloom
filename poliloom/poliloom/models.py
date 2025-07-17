9fe46fd0b2f21ab507a93537c62872b1fa6c1332
Johan Schuijt <johan.schuijt@ftm.nl>
2025-07-17 17:03:06 +0200

    add missing index

--- poliloom/poliloom/models.py
+++ poliloom/poliloom/models.py
@@ -1,7 +1,15 @@
 """Database models for the PoliLoom project."""
 
 from datetime import datetime, timezone
-from sqlalchemy import Column, String, DateTime, ForeignKey, Integer, Enum
+from sqlalchemy import (
+    Column,
+    String,
+    DateTime,
+    ForeignKey,
+    Integer,
+    Enum,
+    UniqueConstraint,
+)
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship, declarative_base
 from uuid import uuid4
@@ -125,6 +133,9 @@ class Property(Base, TimestampMixin):
     """Property entity for storing extracted politician properties."""
 
     __tablename__ = "properties"
+    __table_args__ = (
+        UniqueConstraint("politician_id", "type", name="uq_properties_politician_type"),
+    )
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
     politician_id = Column(
