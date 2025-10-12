dc2fbf69df36e129b115b6be559b109151edee40
Johan <johan@resolve.works>
2025-10-12 11:28:48 +0200

    redirect home on logout

--- poliloom-gui/src/components/Header.tsx
+++ poliloom-gui/src/components/Header.tsx
@@ -35,7 +35,7 @@ export function Header() {
                   Preferences
                 </Button>
                 <Button
-                  onClick={() => signOut()}
+                  onClick={() => signOut({ callbackUrl: '/' })}
                   variant="ghost"
                   size="sm"
                   className="text-gray-500 hover:text-gray-700"
