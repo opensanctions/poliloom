35fc14160ba50b8916b83705e05b896de097f980
Johan Schuijt <johan@resolve.works>
2025-11-23 16:40:38 +0100

    move archived page to its own test

--- poliloom/tests/models/test_politician.py
+++ poliloom/tests/models/test_politician.py
@@ -8,7 +8,6 @@ from poliloom.models import (
     Country,
     WikidataRelation,
     RelationType,
-    ArchivedPage,
 )
 from poliloom.wikidata_date import WikidataDate
 
@@ -1413,47 +1412,3 @@ class TestPropertyShouldStore:
         )
 
         assert new_property.should_store(db_session) is False
-
-
-class TestArchivedPage:
-    """Test cases for the ArchivedPage model."""
-
-    def test_create_references_json_with_wikipedia_project(
-        self, db_session, sample_wikipedia_project
-    ):
-        """Test that create_references_json uses P143 for Wikipedia projects."""
-        # Create an archived page with a Wikipedia project ID
-        archived_page = ArchivedPage(
-            url="https://en.wikipedia.org/wiki/Example",
-            fetch_timestamp=datetime.now(timezone.utc),
-            wikipedia_project_id=sample_wikipedia_project.wikidata_id,
-        )
-        db_session.add(archived_page)
-        db_session.flush()
-
-        references_json = archived_page.create_references_json()
-
-        # Should use P143 (imported from) for Wikipedia projects
-        assert len(references_json) == 1
-        assert references_json[0]["property"]["id"] == "P143"
-        assert references_json[0]["value"]["type"] == "value"
-        assert references_json[0]["value"]["content"] == "Q328"
-
-    def test_create_references_json_without_wikipedia_project(self, db_session):
-        """Test that create_references_json uses P854 for non-Wikipedia sources."""
-        # Create an archived page without a Wikipedia project ID
-        archived_page = ArchivedPage(
-            url="https://example.com/article",
-            fetch_timestamp=datetime.now(timezone.utc),
-            wikipedia_project_id=None,
-        )
-        db_session.add(archived_page)
-        db_session.flush()
-
-        references_json = archived_page.create_references_json()
-
-        # Should use P854 (reference URL) for non-Wikipedia sources
-        assert len(references_json) == 1
-        assert references_json[0]["property"]["id"] == "P854"
-        assert references_json[0]["value"]["type"] == "value"
-        assert references_json[0]["value"]["content"] == "https://example.com/article"
